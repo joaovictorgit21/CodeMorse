e836139f37a7cb88268121535a8bd06a29e5dd8e
João Victor <joaovictorgit21@gmail.com>
2023-09-13 09:59:19 -0300

    Page em construção - Em breve

---
+++ components/js/app.js
@@ -0,0 +1,50 @@
+const morseCodeMap = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', ' ': '/', '?':'..--..', '!':'-.-.--', '.':'.-.-.-', ',':'--..--',
+    ';':'-.-.-.', ':':'---...', '+':'.-.-.', '-':'-....-', '=':'-...-'
+  };
+
+  function textToMorse(text) {
+    return text.toUpperCase().split('').map(char => {
+      if (morseCodeMap[char]) {
+        return morseCodeMap[char];
+      } else {
+        return char;
+      }
+    }).join(' ');
+  }
+
+  function morseToText(morse) {
+    return morse.split(' ').map(code => {
+      for (let char in morseCodeMap) {
+        if (morseCodeMap[char] === code) {
+          return char;
+        }
+      }
+      return code;
+    }).join('');
+  }
+
+  function convertToMorse() {
+    const inputText = document.getElementById('inputText').value;
+    const morse = textToMorse(inputText);
+    document.getElementById('inputMorse').value = morse;
+  }
+
+  function convertToText() {
+    const inputMorse = document.getElementById('inputMorse').value;
+    const text = morseToText(inputMorse);
+    document.getElementById('output').textContent = `Texto decodificado: ${text}`;
+  }
+
+document.addEventListener('keypress', (e) => {
+  if (e.key  === 'Enter') {
+    convertToMorse();
+  };
+});
